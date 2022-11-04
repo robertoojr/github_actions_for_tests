fe5d24507e6ae4896bcdf4efba36eef12f821e75
robertoojr <robertoojr>
2022-11-04 02:57:35 +0000

    add tests

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
+# Databricks notebook source
 from functions import *
+
+# COMMAND ----------
 
 def test_sum():
     result = sum_numbers(3, 2)
     assert result == 5
+
+# COMMAND ----------
 
 def test_subtract():
     result = subtract_numbers(3, 2)
